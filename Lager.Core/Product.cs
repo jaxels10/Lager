4357507564a8c76b48121e4fbf21857cef479ac8
jaxels10 <jaxels10@student.aau.dk>
2020-07-24 23:05:52 +0200

    test commit

--- Lager.Core/Product.cs
+++ Lager.Core/Product.cs
@@ -7,10 +7,12 @@ namespace Lager.Core
     class Product
     {
         public int price;
+        public string name; 
 
-        public Product(int price)
+        public Product(int price, string name)
         {
-            this.price = price; 
+            this.price = price;
+            this.name = name; 
         }
     }
 }
