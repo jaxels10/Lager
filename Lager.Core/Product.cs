2cb87ae5bdb3db0db4358c10539e77a39d249cd9
jaxels10 <jaxels10@student.aau.dk>
2020-07-24 23:04:19 +0200

    hej

---
+++ Lager.Core/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lager.Core
+{
+    class Product
+    {
+        public int price;
+
+        public Product(int price)
+        {
+            this.price = price; 
+        }
+    }
+}
